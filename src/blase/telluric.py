aeb2b23ac9d95a2be709145ebe7d62a37a3300d2
gully <igully@gmail.com>
2020-12-03 09:44:01 -0600

    First commit of telluric model

---
+++ src/blase/telluric.py
@@ -0,0 +1,30 @@
+"""
+telluric
+--------
+
+Telluric absorption forward modeling based on HITRAN
+
+TelluricModel
+############
+"""
+
+import torch
+from torch import nn
+
+
+# custom dataset loader
+class TelluricModel(nn.Module):
+    r"""Make a model of Earth's atmospheric absorption and/or sky emission
+
+    Args:
+        device (str): On which device to run the model, "cuda" or "cpu"
+
+    """
+
+    def __init__(self, device="cuda"):
+        super().__init__()
+        self.device = device
+
+    def forward(self):
+        """The forward pass of the neural network"""
+        return torch.ones(2048, device=self.device, dtype=torch.float64)
