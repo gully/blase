f7bb6c8f410daf2d8aa31c3a2f0aac9fb6539122
gully <igully@gmail.com>
2022-03-18 15:10:09 -0500

    Add a pass through for initializing the model with existing statedict

--- src/blase/emulator.py
+++ src/blase/emulator.py
@@ -4,6 +4,7 @@ Emulator
 
 Precomputed synthetic spectral models are awesome but imperfect and rigid.  Here we clone the most prominent spectral lines and continuum appearance of synthetic spectral models to turn them into tunable, flexible, semi-empirical models.  We can ultimately learn the properties of the pre-computed models with a neural network training loop, and then transfer those weights to real data, where a second transfer-learning training step can take place. The spectrum has :math:`N_{\rm pix} \sim 300,000` pixels and :math:`N_{\rm lines} \sim 5000` spectral lines.  The number of lines is set by the `prominence=` kwarg: lower produces more lines and higher (up to about 0.3) produces fewer lines.  
 """
+from cmath import log
 import math
 import torch
 from torch import nn
@@ -27,33 +28,33 @@ class LinearEmulator(nn.Module):
         The vector continuum-flattened input fluxes
     prominence : int
         The threshold prominence for peak finding
+    init_state_dict : dict
+        A dictionary of model parameters to initialize the model with
     """
 
-    def __init__(self, wl_native, flux_native, prominence=0.03):
+    def __init__(self, wl_native, flux_native, prominence=0.03, init_state_dict=None):
         super().__init__()
 
         # Read in the synthetic spectra at native resolution
         self.wl_native = torch.tensor(wl_native)
         self.flux_native = torch.tensor(flux_native)
-
-        (lam_centers, amplitudes, widths_angstroms,) = self.detect_lines(
-            self.wl_native, self.flux_native, prominence=prominence
-        )
-
-        self.n_pix = len(wl_native)
-
-        line_buffer = 30  # Angstroms
         self.wl_min = wl_native.min()
         self.wl_max = wl_native.max()
+        self.n_pix = len(wl_native)
 
         ## Set up "active area", where the region-of-interest is:
+        ## Restrict the lines to the active region plus 30 A buffer region
+        ## These are hardcoded, and care should be taken if changing them
+        line_buffer = 30  # Angstroms
         active_buffer = 60  # Angstroms
+
         active_lower, active_upper = (
             self.wl_min + active_buffer,
             self.wl_max - active_buffer,
         )
         active_mask = (wl_native > active_lower) & (wl_native < active_upper)
         self.active_mask = torch.tensor(active_mask)
+
         self.flux_active = self.flux_native[active_mask]
         self.wl_active = self.wl_native[active_mask]
 
@@ -63,42 +64,47 @@ class LinearEmulator(nn.Module):
             self.wl_max - line_buffer,
         )
 
-        # Restrict the lines to the active region plus 30 A buffer region
+        if init_state_dict is not None:
+            lam_centers = init_state_dict["lam_centers"]
+            log_amps = init_state_dict["amplitudes"]
+            log_sigma_widths = init_state_dict["sigma_widths"]
+            log_gamma_widths = init_state_dict["gamma_widths"]
+        else:
+            (lam_centers, amplitudes, widths_angstroms,) = self.detect_lines(
+                self.wl_native, self.flux_native, prominence=prominence
+            )
 
-        mask = (lam_centers > line_threshold_lower) & (
-            lam_centers < line_threshold_upper
-        )
-        lam_centers = lam_centers[mask]
-        amplitudes = amplitudes[mask]
-        widths_angstroms = widths_angstroms[mask]
-        self.n_lines = len(lam_centers)
+            # Experimentally determined scale factor tweaks
+            amp_tweak = 0.14
+            sigma_width_tweak = 1.28
+            gamma_width_tweak = 1.52
 
-        # Experimentally determined scale factor tweaks
-        amp_tweak = 0.14
-        sigma_width_tweak = 1.28
-        gamma_width_tweak = 1.52
-
-        self.amplitudes = nn.Parameter(
-            torch.log(amplitudes * amp_tweak).clone().detach().requires_grad_(True)
-        )
-        self.sigma_widths = nn.Parameter(
-            np.log(widths_angstroms / math.sqrt(2) * sigma_width_tweak)
-            .clone()
-            .detach()
-            .requires_grad_(True)
-        )
-
-        self.gamma_widths = nn.Parameter(
-            np.log(widths_angstroms / math.sqrt(2) * gamma_width_tweak)
-            .clone()
-            .detach()
-            .requires_grad_(True)
-        )
+            mask = (lam_centers > line_threshold_lower) & (
+                lam_centers < line_threshold_upper
+            )
+            lam_centers = lam_centers[mask]
+            log_amps = torch.log(amplitudes[mask] * amp_tweak)
+            log_sigma_widths = torch.log(
+                widths_angstroms[mask] / math.sqrt(2) * sigma_width_tweak
+            )
+            log_gamma_widths = torch.log(
+                widths_angstroms[mask] / math.sqrt(2) * gamma_width_tweak
+            )
 
         # Fix the wavelength centers as gospel for now.
         self.lam_centers = nn.Parameter(
             lam_centers.clone().detach().requires_grad_(False)
         )
+        self.amplitudes = nn.Parameter(log_amps.clone().detach().requires_grad_(True))
+        self.sigma_widths = nn.Parameter(
+            log_sigma_widths.clone().detach().requires_grad_(True)
+        )
+
+        self.gamma_widths = nn.Parameter(
+            log_gamma_widths.clone().detach().requires_grad_(True)
+        )
+
+        self.n_lines = len(lam_centers)
 
         self.a_coeff = nn.Parameter(
             torch.tensor(1.0, requires_grad=False, dtype=torch.float64)
