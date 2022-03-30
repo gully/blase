14a9e3a1920c54c786c3c1df3c7e55498781f482
gully <igully@gmail.com>
2022-03-30 14:12:12 -0500

    Add a Doppler grid sampling utility

--- src/blase/utils.py
+++ src/blase/utils.py
@@ -1,5 +1,8 @@
 from contextlib import contextmanager
 import sys, os
+import numpy as np
+from astropy import constants as const
+from astropy import units as u
 
 
 @contextmanager
@@ -11,4 +14,13 @@ def suppress_stdout():
             yield
         finally:
             sys.stdout = old_stdout
+
+
+def doppler_grid(lambda_0, lambda_max, velocity_resolution_kmps=0.5):
+    """Compute the exponential sampling interval for a given lambda_0 and velocity resolution.
+    """
+    c_kmps = const.c.to(u.km / u.s).value
+    velocity_max = c_kmps * np.log(lambda_max / lambda_0)
+    velocity_vector = np.arange(0, velocity_max, velocity_resolution_kmps)
+    return lambda_0 * np.exp(velocity_vector / c_kmps)
 
