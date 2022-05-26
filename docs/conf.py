fae0c9ee710e91fe11b5eed214c998056c06a32c
gully <igully@gmail.com>
2022-05-26 15:06:03 -0500

    The tutorial cells were not syntax hilighting for some reason...

--- docs/conf.py
+++ docs/conf.py
@@ -67,6 +67,8 @@ nbsphinx_thumbnails = {
 #
 html_theme = "sphinx_book_theme"
 
+pygments_style = "vs"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
