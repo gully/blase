6bc71ad9d2fd21de1f895b95936837d0b389cbf1
Sujay Shankar <sujay.shankar@utexas.edu>
2023-09-07 09:02:23 -0500

    Create optimize script for spectra-by-spectra optimization

---
+++ src/blase/optimizer.py
@@ -0,0 +1,30 @@
+from blase.emulator import SparseLinearEmulator as SLE
+from gollum.phoenix import *
+from sys import argv
+
+extract = lambda x: x.detach().cpu().numpy()
+
+def main(spec: PHOENIXSpectrum, 
+         wing_cut: int = 6000, 
+         prominence: float = 0.005, 
+         epochs: int = 100, 
+         LR: float = 0.05, 
+         device: str = 'cpu'):
+    # Preprocessing step
+    norm_spec = spec.divide_by_blackbody().normalize()
+    continuum = norm_spec.fit_continuum(polyorder=5)
+    clean_spec = norm_spec.divide(continuum, handle_meta='ff')
+    # Create emulator
+    emulator = SLE(*clean_spec.data, prominence, device, wing_cut)
+    emulator.to(device)
+    # Obtain relevant data
+    centers = extract(emulator.lam_centers)
+    emulator.optimize(epochs=epochs, LR=LR)
+    amps = extract(emulator.amplitudes)
+    sigmas = extract(emulator.sigma_widths)
+    gammas = extract(emulator.gamma_widths)
+    
+    return centers, amps, sigmas, gammas
+
+if __name__ == '__main__':
+    main(argv[1:])
