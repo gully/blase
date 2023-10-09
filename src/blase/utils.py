971b32d8a899e5daa9823fcdd8817f6a40151b93
Sujay Shankar <sujay.shankar@utexas.edu>
2023-10-09 06:36:45 -0500

    Paper changes, torch device util, try .env

--- src/blase/utils.py
+++ src/blase/utils.py
@@ -4,23 +4,24 @@ utilities
 
 These general purpose methods are used in one or more other modules.
 """
-from contextlib import contextmanager
-import sys, os
 import numpy as np
+import torch
+
 from astropy import constants as const
 from astropy import units as u
+from contextlib import contextmanager
+from os import devnull
+from sys import stdout
 
 
 @contextmanager
 def suppress_stdout():
-    with open(os.devnull, "w") as devnull:
-        old_stdout = sys.stdout
-        sys.stdout = devnull
+    with open(devnull, "w") as devnull:
+        old_stdout, stdout = stdout, devnull
         try:
             yield
         finally:
-            sys.stdout = old_stdout
-
+            stdout = old_stdout
 
 def doppler_grid(lambda_0, lambda_max, velocity_resolution_kmps=0.5):
     """Compute the exponential sampling interval for a given lambda_0 and velocity resolution."""
@@ -28,3 +29,12 @@ def doppler_grid(lambda_0, lambda_max, velocity_resolution_kmps=0.5):
     velocity_max = c_kmps * np.log(lambda_max / lambda_0)
     velocity_vector = np.arange(0, velocity_max, velocity_resolution_kmps)
     return lambda_0 * np.exp(velocity_vector / c_kmps)
+
+def auto_torch_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    else:
+        try:
+            return torch.device("mps")
+        except RuntimeError:
+            return torch.device("cpu")
