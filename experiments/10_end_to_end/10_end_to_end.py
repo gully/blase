46381a02ca10bd12305e4bb841d429da7b98670b
Sujay Shankar <sujays2001@gmail.com>
2024-04-09 18:38:03 -0400

    Change interpolator backend to RegularGridInterpolator

--- experiments/10_end_to_end/10_end_to_end.py
+++ experiments/10_end_to_end/10_end_to_end.py
@@ -13,7 +13,7 @@ from os import listdir
 from pickle import dump, load
 from re import split
 from skopt import gp_minimize
-from scipy.interpolate import griddata
+from scipy.interpolate import griddata, RegularGridInterpolator
 from tqdm import tqdm
 from typing import Callable
 
@@ -43,7 +43,11 @@ def create_interpolators(df: pd.DataFrame, df_gp: pd.DataFrame) -> list[partial]
     interpolator_list = []
     for line in tqdm(df.center.unique()):
         df_line = df.query('center == @line', engine='python').merge(df_gp, how='right', on=['teff', 'logg', 'Z']).fillna(-1000)
-        interpolator_list.append(partial(griddata, points=(df_line.teff, df_line.logg, df_line.Z), values=df_line[['amp', 'sigma', 'gamma', 'shift_center']].to_numpy()))
+        interpolator_list.append(RegularGridInterpolator(
+            (df_line.teff.unique(), df_line.logg.unique(), df_line.Z.unique()), 
+            df_line[['amp', 'sigma', 'gamma', 'shift_center']].to_numpy().reshape(len(df_line.teff.unique()), len(df_line.logg.unique()), len(df_line.Z.unique()), 4)),
+            method='linear')
+        #interpolator_list.append(partial(griddata, points=(df_line.teff, df_line.logg, df_line.Z), values=df_line[['amp', 'sigma', 'gamma', 'shift_center']].to_numpy()))
     return interpolator_list
 
 def reconstruct(wl_grid: np.ndarray, point: tuple[int, float, float]) -> np.ndarray:
@@ -60,60 +64,8 @@ def reconstruct(wl_grid: np.ndarray, point: tuple[int, float, float]) -> np.ndar
 def chisq_loss(wl_grid: np.ndarray, data: np.ndarray) -> Callable:
     return lambda point: np.sum((reconstruct(wl_grid, point) - data)**2)
 
-def local_run(p_teff, p_logg, p_Z):
-    path = '/home/sujay/data/10K_12.5K_clones'
-    df_native = read_state_dicts(path).query('-0.5 <= Z <= 0.5 & 4000 <= teff <= 7000 & 2 <= logg <= 4')
-    df_gp = df_native[['teff', 'logg', 'Z']]
-    df = df_native.explode(['center', 'amp', 'sigma', 'gamma', 'shift_center']).convert_dtypes(dtype_backend='numpy_nullable')
-    print('DataFrame created')
-    optimize_memory(df)
-    print('DataFrame memory optimized')
-    interpolator_list = []
-    for line in tqdm(df.center.unique()[:2000]):
-        df_line = df.query('center == @line')
-        df_line = df_line.merge(df_gp, how='right', on=['teff', 'logg', 'Z']).fillna(-1000)
-        interpolator_list.append(partial(griddata, points=(df_line.teff, df_line.logg, df_line.Z), values=df_line[['amp', 'sigma', 'gamma', 'shift_center']].to_numpy(), method='linear'))
-
-    print('Interpolator partials created')
-    output = np.vstack([r for interpolator in interpolator_list if (r := interpolator(xi=(p_teff, p_logg, p_Z)))[0] != -1000])
-    state_dict = {
-        'pre_line_centers': torch.from_numpy(df.center.unique()[:2000].to_numpy(dtype=np.float64)),
-        'amplitudes': torch.from_numpy(output[:, 0]),
-        'sigma_widths': torch.from_numpy(output[:, 1]),
-        'gamma_widths': torch.from_numpy(output[:, 2]),
-        'lam_centers': torch.from_numpy(output[:, 3]),
-    }
-    wl_lo = 8038
-    wl_hi = 12849
-    grid = doppler_grid(wl_lo, wl_hi)
-    emulator1 = np.nan_to_num(SLE(wl_native=grid, init_state_dict=state_dict, device="cpu").forward().detach().numpy(), nan=1)
-
-    df_native = read_state_dicts(path).query('-0.5 <= Z <= 0.5 & 4000 <= teff <= 7000 & 2 <= logg <= 4 & teff != @p_teff & logg != @p_logg & Z != @p_Z')
-    df_gp = df_native[['teff', 'logg', 'Z']]
-    df = df_native.explode(['center', 'amp', 'sigma', 'gamma', 'shift_center']).convert_dtypes(dtype_backend='numpy_nullable')
-    print('DataFrame created')
-    optimize_memory(df)
-    print('DataFrame memory optimized')
-    interpolator_list = []
-    for line in tqdm(df.center.unique()[:2000]):
-        df_line = df.query('center == @line')
-        df_line = df_line.merge(df_gp, how='right', on=['teff', 'logg', 'Z']).fillna(-1000)
-        interpolator_list.append(partial(griddata, points=(df_line.teff, df_line.logg, df_line.Z), values=df_line[['amp', 'sigma', 'gamma', 'shift_center']].to_numpy(), method='linear'))
-
-    print('Interpolator partials created')
-    output = np.vstack([r for interpolator in interpolator_list if (r := interpolator(xi=(p_teff, p_logg, p_Z)))[0] != -1000])
-    state_dict = {
-        'pre_line_centers': torch.from_numpy(df.center.unique()[:2000].to_numpy(dtype=np.float64)),
-        'amplitudes': torch.from_numpy(output[:, 0]),
-        'sigma_widths': torch.from_numpy(output[:, 1]),
-        'gamma_widths': torch.from_numpy(output[:, 2]),
-        'lam_centers': torch.from_numpy(output[:, 3]),
-    }
-    emulator2 = np.nan_to_num(SLE(wl_native=grid, init_state_dict=state_dict, device="cpu").forward().detach().numpy(), nan=1)
-    pd.DataFrame({'wavelength': grid, 'flux1': emulator1, 'flux2': emulator2}).to_parquet('plot_residual_hist.parquet.gz', compression='gzip')
-
 def triton_run():
-    sys.stderr = open('log', 'w')
+    sys.stderr = open('log.txt', 'w')
     path = '/home/sujays/github/blase/experiments/08_blase3D_HPC_test/emulator_states'
     df = read_state_dicts(path)
     df_gp = df[['teff', 'logg', 'Z']]
@@ -122,19 +74,20 @@ def triton_run():
     optimize_memory(df)
     print('DataFrame memory optimized')
     interpolators = create_interpolators(df, df_gp)
-    dump(interpolators, open('interpolators.pkl', 'wb'))
-    with open('log.txt') as f:
+    dump(interpolators, open('interpolator_list.pkl', 'wb'))
+    with open('log.txt', 'w') as f:
         f.write('Interpolator partials dumped to pickle.')
 
 def inference_run():
     spec = PHOENIXSpectrum(teff=5000, logg=4, Z=0, download=True)
-    res = gp_minimize(chisq_loss(spec.wavelength.value, spec.flux.value), dimensions=[(2300, 12000), (0, 5), (-0.5, 0.5)], n_calls=100, n_random_starts=10, random_state=0, n_jobs=16)
+    res = gp_minimize(chisq_loss(spec.wavelength.value, spec.flux.value), dimensions=[(2300, 12000), (0, 5), (-0.5, 0.5)], n_calls=20, n_random_starts=20)
     print(res.x)
     print(res.x_iters)
     print(res.fun)
     print(res.func_vals)
 
 if __name__ == '__main__':
-    spec = PHOENIXSpectrum(teff=5000, logg=4, Z=0, download=True)
-    x = chisq_loss(spec.wavelength.value, spec.flux.value)((5000, 4, 0))
-    print(x)
+    #df = pd.DataFrame({'x': [0, 0, 1, 1], 'y': [2, 3, 2, 3], 'u': [4, 5, 6, 7], 'v': [8, 9, 10, 11], 'w': [12, 13, 14, 15]})
+    #interp2 = RegularGridInterpolator((df.x.unique(), df.y.unique()), df[['u', 'v', 'w']].to_numpy().reshape(2, 2, 3))
+    #print(interp2([0, 3]))
+    triton_run()
