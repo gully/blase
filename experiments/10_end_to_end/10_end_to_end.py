146fe60f0c73ade7bab0123fb02010b693f69baa
Sujay Shankar <sujay.shankar@utexas.edu>
2024-04-12 17:48:52 +0000

    Successfully finish single inference

--- experiments/10_end_to_end/10_end_to_end.py
+++ experiments/10_end_to_end/10_end_to_end.py
@@ -43,16 +43,17 @@ def create_interpolators(df: pd.DataFrame, df_gp: pd.DataFrame) -> list[partial]
     interpolator_list = []
     for line in tqdm(df.center.unique()):
         df_line = df.query('center == @line', engine='python').merge(df_gp, how='right', on=['teff', 'logg', 'Z']).fillna(-1000)
+        df_line.sort_values(['teff', 'logg', 'Z'], inplace=True)
         interpolator_list.append(RegularGridInterpolator(
             (df_line.teff.unique(), df_line.logg.unique(), df_line.Z.unique()), 
-            df_line[['amp', 'sigma', 'gamma', 'shift_center']].to_numpy().reshape(len(df_line.teff.unique()), len(df_line.logg.unique()), len(df_line.Z.unique()), 4)),
-            method='linear')
+            df_line[['amp', 'sigma', 'gamma', 'shift_center']].to_numpy().reshape(len(df_line.teff.unique()), len(df_line.logg.unique()), len(df_line.Z.unique()), 4),
+            method='linear'))
         #interpolator_list.append(partial(griddata, points=(df_line.teff, df_line.logg, df_line.Z), values=df_line[['amp', 'sigma', 'gamma', 'shift_center']].to_numpy()))
     return interpolator_list
 
 def reconstruct(wl_grid: np.ndarray, point: tuple[int, float, float]) -> np.ndarray:
-    interpolators = load(open('interpolators.pkl', 'rb'))
-    output = np.vstack([r for interpolator in interpolators if (r := interpolator(xi=(point[0], point[1], point[2])))[0] != -1000])
+    interpolators = load(open('interpolator_list.pkl', 'rb'))
+    output = np.vstack([r for interpolator in interpolators if (r := interpolator([point[0], point[1], point[2]]).squeeze())[0] != -1000])
     state_dict = {
         'amplitudes': torch.from_numpy(output[:, 0]),
         'sigma_widths': torch.from_numpy(output[:, 1]),
@@ -61,8 +62,8 @@ def reconstruct(wl_grid: np.ndarray, point: tuple[int, float, float]) -> np.ndar
     }
     return np.nan_to_num(SLE(wl_native=wl_grid, init_state_dict=state_dict, device="cpu").forward().detach().numpy(), nan=1)
 
-def chisq_loss(wl_grid: np.ndarray, data: np.ndarray) -> Callable:
-    return lambda point: np.sum((reconstruct(wl_grid, point) - data)**2)
+def loss_fn(wl_grid: np.ndarray, data: np.ndarray) -> Callable:
+    return lambda point: ((reconstruct(wl_grid, point) - data)**2).mean()**0.5
 
 def triton_run():
     sys.stderr = open('log.txt', 'w')
@@ -79,15 +80,15 @@ def triton_run():
         f.write('Interpolator partials dumped to pickle.')
 
 def inference_run():
-    spec = PHOENIXSpectrum(teff=5000, logg=4, Z=0, download=True)
-    res = gp_minimize(chisq_loss(spec.wavelength.value, spec.flux.value), dimensions=[(2300, 12000), (0, 5), (-0.5, 0.5)], n_calls=20, n_random_starts=20)
-    print(res.x)
-    print(res.x_iters)
-    print(res.fun)
-    print(res.func_vals)
+    from blase.optimizer import default_clean
+    from time import perf_counter
+    start = perf_counter()
+    spec = default_clean(PHOENIXSpectrum(teff=5000, logg=4, Z=0, download=True))
+    res = gp_minimize(loss_fn(spec.wavelength.value, spec.flux.value), dimensions=[(2300, 12000), (2, 6), (-0.5, 0)], n_calls=50, n_random_starts=30)
+    print(f'Result: {res.x} achieved in {perf_counter() - start} s')
 
 if __name__ == '__main__':
     #df = pd.DataFrame({'x': [0, 0, 1, 1], 'y': [2, 3, 2, 3], 'u': [4, 5, 6, 7], 'v': [8, 9, 10, 11], 'w': [12, 13, 14, 15]})
     #interp2 = RegularGridInterpolator((df.x.unique(), df.y.unique()), df[['u', 'v', 'w']].to_numpy().reshape(2, 2, 3))
     #print(interp2([0, 3]))
-    triton_run()
+    inference_run()
