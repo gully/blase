85fe21115baf2810f5b41429ce99e81d9609d122
Sujay Shankar <sujay.shankar@utexas.edu>
2024-04-19 13:20:36 -0400

    Add figures and math to paper

--- experiments/10_end_to_end/10_end_to_end.py
+++ experiments/10_end_to_end/10_end_to_end.py
@@ -48,19 +48,6 @@ def create_interpolators(df: pd.DataFrame, df_gp: pd.DataFrame) -> list[RegularG
             method='linear'))
     return interpolator_list
 
-def reconstruct(wl_grid: np.ndarray, point: tuple[int, float, float], interpolator_list: list[RegularGridInterpolator]) -> np.ndarray:
-    output = np.vstack([r for interpolator in interpolator_list if (r := interpolator([point[0], point[1], point[2]]).squeeze())[0] != -1000])
-    state_dict = {
-        'amplitudes': torch.from_numpy(output[:, 0]),
-        'sigma_widths': torch.from_numpy(output[:, 1]),
-        'gamma_widths': torch.from_numpy(output[:, 2]),
-        'lam_centers': torch.from_numpy(output[:, 3]),
-    }
-    return np.nan_to_num(SLE(wl_native=wl_grid, init_state_dict=state_dict).forward().detach().numpy(), nan=1)
-
-def loss_fn(wl_grid: np.ndarray, data: np.ndarray, interpolator_list: list[RegularGridInterpolator]) -> Callable:
-    return lambda point: ((reconstruct(wl_grid, point, interpolator_list) - data)**2).mean()**0.5
-
 def pickling_run():
     sys.stderr = open('log.txt', 'w')
     path = '/home/sujays/github/blase/experiments/08_blase3D_HPC_test/emulator_states'
@@ -74,6 +61,25 @@ def pickling_run():
     dump(interpolators, open('interpolator_list.pkl', 'wb'))
     with open('log.txt', 'w') as f:
         f.write('Interpolator partials dumped to pickle.')
+
+def reconstruct(wl_grid: np.ndarray, point: tuple[int, float, float], interpolator_list: list[RegularGridInterpolator]) -> np.ndarray:
+    output = np.vstack([r for interpolator in interpolator_list if (r := interpolator([point[0], point[1], point[2]]).squeeze())[0] != -1000])
+    state_dict = {
+        'amplitudes': torch.from_numpy(output[:, 0]),
+        'sigma_widths': torch.from_numpy(output[:, 1]),
+        'gamma_widths': torch.from_numpy(output[:, 2]),
+        'lam_centers': torch.from_numpy(output[:, 3]),
+    }
+    return np.nan_to_num(SLE(wl_native=wl_grid, init_state_dict=state_dict).forward().detach().numpy(), nan=1)
+
+def random_starts(wl_grid: np.ndarray, data: np.ndarray, interpolator_list: list[RegularGridInterpolator], n: int) -> tuple[np.ndarray, np.ndarray]:
+    random_teff =  np.random.randint(2300, 12000, n)
+    random_logg = np.random.uniform(2, 6, n)
+    random_Z = np.random.uniform(-0.5, 0, n)
+    
+
+def loss_fn(wl_grid: np.ndarray, data: np.ndarray, interpolator_list: list[RegularGridInterpolator]) -> Callable:
+    return lambda point: ((reconstruct(wl_grid, point, interpolator_list) - data)**2).mean()**0.5
 
 def inference_test():
     from time import perf_counter
@@ -99,4 +105,8 @@ def inference_grid():
 
 
 if __name__ == '__main__':
-    inference_test()
+    interpolator = load(open('interpolator_list.pkl', 'rb'))[10051]
+    grid = PHOENIXGrid(teff_range=(2300, 12000), logg_range=(2, 6), Z_range=(0, 0), path='/data/libraries/raw/PHOENIX/')
+    output = [interpolator(point).squeeze()[0] for point in grid.grid_points]
+    df = pd.DataFrame({'teff': [p[0] for p in grid.grid_points], 'logg': [p[1] for p in grid.grid_points], 'Z': [p[2] for p in grid.grid_points], 'amp': output})
+    print(df)
