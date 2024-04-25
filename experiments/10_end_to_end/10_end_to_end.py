45ab4649ffa613d34ccd512765df7e1894826478
Sujay Shankar <sujay.shankar@utexas.edu>
2024-04-25 11:17:41 -0400

    Change paper and create multi-reconstructor

--- experiments/10_end_to_end/10_end_to_end.py
+++ experiments/10_end_to_end/10_end_to_end.py
@@ -7,12 +7,12 @@ from blase.emulator import SparseLinearEmulator as SLE
 from blase.optimizer import default_clean
 from collections import defaultdict
 from gollum.phoenix import PHOENIXSpectrum, PHOENIXGrid
-from itertools import product
 from os import listdir
 from pickle import dump, load
 from re import split
 from skopt import gp_minimize
 from scipy.interpolate import RegularGridInterpolator
+from time import perf_counter
 from tqdm import tqdm
 from typing import Callable
 
@@ -64,8 +64,8 @@ def pickling_run():
     dump(interpolators, open('interpolator_list.pkl', 'wb'))
     print(f'Interpolator partials dumped to pickle ({perf_counter() - start} s).')
 
-def reconstruct(wl_grid: np.ndarray, point: tuple[int, float, float], interpolator_list: list[RegularGridInterpolator]) -> np.ndarray:
-    output = np.vstack([r for interpolator in interpolator_list if (r := interpolator([point[0], point[1], point[2]]).squeeze())[0] != -1000])
+def reconstruct1(wl_grid: np.ndarray, point: np.ndarray, interpolator_list: list[RegularGridInterpolator]) -> np.ndarray:
+    output = np.vstack([r for interpolator in interpolator_list if (r := interpolator(point).squeeze())[0] != -1000])
     state_dict = {
         'amplitudes': torch.from_numpy(output[:, 0]),
         'sigma_widths': torch.from_numpy(output[:, 1]),
@@ -74,44 +74,39 @@ def reconstruct(wl_grid: np.ndarray, point: tuple[int, float, float], interpolat
     }
     return np.nan_to_num(SLE(wl_native=wl_grid, init_state_dict=state_dict).forward().detach().numpy(), nan=1)
 
-def random_starts(wl_grid: np.ndarray, data: np.ndarray, interpolator_list: list[RegularGridInterpolator], n: int) -> tuple[np.ndarray, np.ndarray]:
-    random_teff =  np.random.randint(2300, 12000, n)
-    random_logg = np.random.uniform(2, 6, n)
-    random_Z = np.random.uniform(-0.5, 0, n)
-    
+def reconstructn(wl_grid: np.ndarray, points: np.ndarray, interpolator_list: list[RegularGridInterpolator]) -> np.ndarray:
+    raw_outputs = [[] for _ in points]
+    for interpolator in interpolator_list:
+        results = interpolator(points).squeeze()
+        for i, r in enumerate(results):
+            if r[0] > -100:
+                raw_outputs[i].append(r)
+    outputs = [np.vstack(raw_output) for raw_output in raw_outputs]
+    state_dicts = [{
+        'amplitudes': torch.from_numpy(output[:, 0]),
+        'sigma_widths': torch.from_numpy(output[:, 1]),
+        'gamma_widths': torch.from_numpy(output[:, 2]),
+        'lam_centers': torch.from_numpy(output[:, 3]),
+    } for output in outputs]
+    return np.nan_to_num(np.vstack([SLE(wl_native=wl_grid, init_state_dict=state_dict).forward().detach().numpy() for state_dict in state_dicts]), nan=1)
 
-def loss_fn(wl_grid: np.ndarray, data: np.ndarray, interpolator_list: list[RegularGridInterpolator]) -> Callable:
-    return lambda point: ((reconstruct(wl_grid, point, interpolator_list) - data)**2).mean()**0.5
+def rms_loss(wl_grid: np.ndarray, data: np.ndarray, interpolator_list: list[RegularGridInterpolator]) -> Callable:
+    return lambda point: ((reconstruct1(wl_grid, point, interpolator_list) - data)**2).mean()**0.5
 
 def inference_test():
-    from time import perf_counter
     interpolator_list = load(open('interpolator_list.pkl', 'rb'))
     start = perf_counter()
     spec = default_clean(PHOENIXSpectrum(teff=5000, logg=4, Z=0, download=True))
-    res = gp_minimize(loss_fn(spec.wavelength.value, spec.flux.value, interpolator_list), dimensions=[(2300, 12000), (2, 6), (-0.5, 0)], n_calls=50, n_random_starts=30)
+    res = gp_minimize(rms_loss(spec.wavelength.value, spec.flux.value, interpolator_list), dimensions=[(2300, 12000), (2, 6), (-0.5, 0)], n_calls=50, n_random_starts=30)
     print(f'Result: {res.x} achieved in {perf_counter() - start} s')
-
-def inference(wavelength: np.ndarray, flux: np.ndarray) -> np.ndarray:
-    return np.array(gp_minimize(loss_fn(wavelength, flux), dimensions=[(2300, 12000), (2, 6), (-0.5, 0)], n_calls=50, n_random_starts=30).x)
-
-def inference_grid():
-    sys.stderr = open('log.txt', 'w')
-    interpolator_list = load(open('interpolator_list.pkl', 'rb'))
-    grid = PHOENIXGrid(teff_range=(2300, 12000), logg_range=(2, 6), Z_range=(-0.5, 0), path='/data/libraries/raw/PHOENIX/')
-    df_list = []
-    for point in grid.grid_points:
-        spec = default_clean(grid[grid.lookup_dict[point]])
-        result = np.vstack(inference(spec.wavelength.value, spec.flux.value) for _ in range(10))
-        df_list.append(pd.DataFrame({'teff': point[0], 'logg': point[1], 'Z': point[2], 'i_teff': result[:, 0], 'i_logg': result[:, 1], 'i_Z': result[:, 2]}))
-    pd.concat(df_list).to_parquet('inference_results.parquet.gz', compression='gzip')
 
 
 if __name__ == '__main__':
-    interpolator = load(open('interpolator_list.pkl', 'rb'))[1]
-    grid = PHOENIXGrid(teff_range=(2300, 12000), logg_range=(2, 6), Z_range=(0, 0), path='/data/libraries/raw/PHOENIX/')
-    teff_points = np.arange(2300, 12000, 50)
-    logg_points = np.arange(2, 6, 0.1)
-    domain = np.array(np.meshgrid(teff_points, logg_points)).T.reshape(-1, 2)
-    output = np.vstack([interpolator((*point, 0)).squeeze() for point in domain])
-    df = pd.DataFrame({'teff': [p[0] for p in domain], 'logg': [p[1] for p in domain], 'amp': output[:,0], 'sigma': output[:,1], 'gamma': output[:,2], 'shift_center': output[:,3]}) 
-    df.to_parquet('interpolated_line.parquet.gz', compression='gzip')
+    sys.stderr = sys.stdout = open('log.txt', 'w')
+    interpolator_list = load(open('interpolator_list.pkl', 'rb'))
+    wl = PHOENIXSpectrum(teff=5000, logg=4, Z=0, download=True).wavelength.value
+    points = np.array([[5000, 2, 0], [6000, 3, -0.5], [7000, 4, 0], [8000, 5, -0.5], [9000, 6, 0]])
+    start = perf_counter()
+    x = reconstructn(wl, points, interpolator_list)
+    print(f'{len(points)} points reconstructed in {perf_counter() - start} s')
+
